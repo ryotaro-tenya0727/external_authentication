a0014ce00cc6abc7f73f4234e63a84d3a45b2361
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-05-11 22:28:51 +0900

    ADD: ログインまで実装

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -26,6 +26,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
@@ -271,7 +272,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
+  config.omniauth :github, ENV['GITHUB_CLIENT_ID'], ENV['GITHUB_CLIENT_SECRET'], scope: 'read:user'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
